8d43cba7b64c9618cdbbead9aae20d05b9182826
NyAgoston <xpnyeste@gmail.com>
2022-03-15 23:13:39 +0100

    2022.03.15

--- POSIX/harmadik.c
+++ POSIX/harmadik.c
@@ -4,17 +4,56 @@
 #include <pthread.h>
 #include <time.h>
 
+#define MERET 100
 
+static int tomb[MERET];
+
+static int primek = 0;
+
+void feltolt();
+void prim_tomb();
+void prim(int n);
 
 int main(int argc, char* argv[]){
 
-    srand(time(NULL));
+    feltolt();
 
-    float random_num = rand()%9 + 1;
-    float random_tort = rand()%500 + 1;
-    random_num = random_num + random_tort * 0.01;
+    prim_tomb();
 
-    printf("%f",random_num);
-    
+    printf("Primek szama a tombben: %d",primek);
     return 0;
+    
+}
+void feltolt(){
+    for (int i = 0; i < MERET; i++)
+    {
+        tomb[i] = i;
+    }
+}
+void prim_tomb(){
+    for (int i = 0; i < MERET; i++)
+    {
+        prim(tomb[i]);
+    }
+    
+}
+void prim(int n){
+
+
+    int i, flag = 0;
+
+    if (n == 0 || n == 1)
+    flag = 1;
+
+    for (i = 2; i <= n / 2; ++i) {
+
+        if (n % i == 0) {
+            flag = 1;
+            break;
+        }
+    }
+
+    if (flag == 0){
+        primek += 1;
+    } 
 }
