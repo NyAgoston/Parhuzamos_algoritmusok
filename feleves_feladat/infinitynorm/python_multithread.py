f55b4ee43b48a39f60666a3fbee9f9a2a1c48b9d
NyAgoston <xpnyeste@gmail.com>
2022-04-26 22:27:14 +0200

    updates

--- feleves_feladat/infinitynorm/python_multithread.py
+++ feleves_feladat/infinitynorm/python_multithread.py
@@ -1,27 +1,50 @@
 import threading
+from random import *
+import time
 
-def worker(array):
-    for i in range (len(array)):
-        print(array[i])
-	
-
+def infnorm(matrix,x):
+    endpart = x * (N / num_of_threads)
+    startpart = endpart - (N / num_of_threads)
+    x = startpart;
+    while x < endpart:
+        sum = 0
+        for j in range(N):
+            sum += abs(matrix[i][j])
+        solutions.append(sum);
+        x+=1
+   
 if __name__ == "__main__":
-    
-    array = [1,3,2,5,4,9,6,7,8]
-    
+    global solutions
+   
+    solutions = []
+       
+    N = 100
+    matrix = [[randint(1,100) for i in range(N)] for j in range(N)]
+    num_of_threads = 5
     threads = []
-	# creating threads
-    
-    for i in range(4):
-        threads[i] = threading.Thread(target=worker, args=(array,))
-    
-	# starting threads
-    for i in range(4):
+    start = time.time()
+
+    t1 = threading.Thread(target=infnorm, args=(matrix,1))
+    t2 = threading.Thread(target=infnorm, args=(matrix,2))
+    t3 = threading.Thread(target=infnorm, args=(matrix,3))
+    t4 = threading.Thread(target=infnorm, args=(matrix,4))
+    t5 = threading.Thread(target=infnorm, args=(matrix,5))
+    threads.append(t1)
+    threads.append(t2)
+    threads.append(t3)
+    threads.append(t4)
+    threads.append(t5)
+
+    for i in range(num_of_threads):
         threads[i].start()
     
-    # joining threads
-    for i in range(4):
+    for i in range(num_of_threads):
         threads[i].join()
 
-	# threads completly executed
+    solution = max(solutions)
+    end = time.time()
+
+    print("The matrixes infinit norm is: ",solution) 
+    print("Elapsed time:",end - start)
+
     print("Done!")
